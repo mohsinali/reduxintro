6ab88fe0b0f6f1bd1cbac6b3b6784d2bba82621c
Mohsin Ali <mohammadmohsin@gmail.com>
2017-03-15 14:18:35 +0500

    Selected book not showing

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    {title: 'Javascript: The good times', pages: 101},
+    {title: 'Harry potter', pages: 60},
+    {title: 'Woody Alan', pages: 200},
+    {title: 'Davinci code', pages: 1}
+  ]
+}
